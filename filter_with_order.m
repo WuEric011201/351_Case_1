2aa4387dc21b2ba678265fdb9e4c9d0603813bfc
Jasmine-Cheng <71244331+Jasmine-Cheng@users.noreply.github.com>
2022-03-07 19:36:31 -0600

    Add files via upload
    modified function: filter_with_order()

--- filter_with_order.m
+++ filter_with_order.m
@@ -2,29 +2,32 @@
 % parameter: 
 % signal: signal we want to filter
 % aLow, bLow, aHigh, bHigh: parameters of filter
-% order: number of times the filter in series
-% type: type of filter
-% returned value: filtered signal 
+% order: number of the filters in series
+% type: type of filter (low, high, band)
+% returned value: filtered signal
 
 
 function signal = filter_with_order(signal, aLow, bLow, aHigh, bHigh, order, type)
 
         % filter the input order of times
         for i = 1: order
-    
-            if (strcmp(type, 'l') ==1 )
-                    % pass the low-pass 
+            
+            % low pass
+            if (strcmp(type, 'l') == 1)
                     signal = filter(bLow, aLow, signal);
-                continue; 
             end
-    
-            % pass the high pass
-            signal = filter(bHigh, aHigh, signal);
-    
+            
+            % high pass
+            if (strcmp(type, 'h') == 1)
+                signal = filter(bHigh, aHigh, signal);
+            end
+            
+            % band pass
             if (strcmp(type, 'x') == 1)
-                % pass the low-pass 
-                signal = filter(bLow, aLow,  signal);
+                signal = filter(bLow, aLow, signal);
+                signal = filter(bHigh, aHigh, signal);
             end
+            
         end
 
 end
