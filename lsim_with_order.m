72c6e6b45617da9476ea5b39ccb97f10678c4cfb
Jasmine-Cheng <71244331+Jasmine-Cheng@users.noreply.github.com>
2022-03-09 20:37:47 -0600

    Add files via upload

--- lsim_with_order.m
+++ lsim_with_order.m
@@ -1,7 +1,7 @@
 % This function does the actual filtering using lsim()
 % 
 % parameter: 
-% signal: signal we want to filter
+% input: signal we want to filter
 % aLow, bLow, aHigh, bHigh: parameters of filter
 % order: number of the filters in series
 % type: l: means only passes through low
@@ -9,29 +9,33 @@
 %       x: means passes through low and high
 % t: the time vector of the impulse
 % 
-% returned value: filtered signal
+% returned value: 
+% output: filtered signal
 
-function signal = lsim_with_order(signal, aLow, bLow, aHigh, bHigh, order, type, t)
-
+function output = lsim_with_order(input, aLow, bLow, aHigh, bHigh, order, type, t)
+        
+        signal = input;
+        
         % filter the input order of times
         for i = 1: order
             
             % low pass
             if (strcmp(type, 'l') == 1)
-                signal = lsim(bLow, aLow, signal, t);
+                signal = lsim(bLow, aLow, signal', t);
             end
             
             % high pass
             if (strcmp(type, 'h') == 1)
-                signal = lsim(bHigh, aHigh, signal, t);
+                signal = lsim(bHigh, aHigh, signal', t);
             end
             
             % band pass
             if (strcmp(type, 'x') == 1)
-                signal = lsim(bLow, aLow, signal, t);
-                signal = lsim(bHigh, aHigh, signal, t);
+                signal = lsim(bLow, aLow, signal', t);
+                signal = lsim(bHigh, aHigh, signal', t);
             end
             
         end
-
+        
+        output =  signal';
 end
